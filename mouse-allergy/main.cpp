1eec3f986506c1d7ee241723fadf8ef6cf20fa4d
Michael Fadely <michaelwfadely@gmail.com>
2016-10-18 18:04:58 -0600

    this is fine I guess

--- mouse-allergy/main.cpp
+++ mouse-allergy/main.cpp
@@ -1,41 +1,47 @@
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
 
+#include <math.h>
+
 static POINT cursor = {};
+static const auto THRESHOLD = 16.0f;
+
+int RunAway(const long& cursor, const long& base, const long &center)
+{
+	auto dist = abs(cursor - center);
+	auto window_center = center - base;
+
+	auto m = (window_center - dist) / (float)window_center;
+	if (cursor >= center)
+		m = -m;
+
+	return (int)(THRESHOLD * m);
+}
 
 BOOL CALLBACK EnumWindowsProc(HWND hwnd, LPARAM lParam)
 {
 	RECT rect;
 	GetWindowRect(hwnd, &rect);
 
+	if (cursor.x < rect.left || cursor.x > rect.right
+		|| cursor.y < rect.top || cursor.y > rect.bottom)
+	{
+		return true;
+	}
+
 	int x = rect.left;
 	int y = rect.top;
 
-	if (cursor.x > rect.left && cursor.x < rect.right)
+	if (cursor.x >= rect.left && cursor.x <= rect.right)
 	{
 		auto center = rect.left + (rect.right - rect.left) / 2;
-
-		if (cursor.x < center)
-		{
-			x += 8;
-		}
-		else
-		{
-			x -= 8;
-		}
+		x += RunAway(cursor.x, rect.left, center);
 	}
 
-	if (cursor.y > rect.top && cursor.y < rect.bottom)
+	if (cursor.y >= rect.top && cursor.y <= rect.bottom)
 	{
-		auto center = rect.top + ((rect.bottom - rect.top) / 2);
-		if (cursor.y < center)
-		{
-			y += 8;
-		}
-		else
-		{
-			y -= 8;
-		}
+		auto center = rect.top + (rect.bottom - rect.top) / 2;
+		y += RunAway(cursor.y, rect.top, center);
 	}
 
 	if (x != rect.left || y != rect.top)
@@ -50,15 +56,15 @@ void main()
 {
 	while (true)
 	{
-		GetCursorPos(&cursor);
-		EnumWindows(EnumWindowsProc, 0);
-
-		auto whatever = GetAsyncKeyState(VK_ESCAPE);
-		if ((1 << 16) & whatever)
+		// ABORT ABORT ABORT
+		auto helpme = GetAsyncKeyState(VK_ESCAPE);
+		if ((1 << 16) & helpme)
 		{
 			return;
 		}
 
+		GetCursorPos(&cursor);
+		EnumWindows(EnumWindowsProc, 0);
 		Sleep(1);
 	}
 }
